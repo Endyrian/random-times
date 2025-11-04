390e5818e5526915b9d0796ec691e14b6980d2da
Endyrian <nathanenderman@gmail.com>
2025-11-03 16:16:20 -0800

    Added code to prevent printing of times equal to 0

--- rand_times.py
+++ rand_times.py
@@ -7,10 +7,14 @@ max_seconds = math.floor(max_days * 86400) #24*60*60=86400
 times = [random.randint(1, max_seconds)]
 while times[-1] > 1:
     times.append(random.randrange(times[-1]))
+if times[-1] == 0:
+    times.pop()
 
 x = max(math.ceil(math.log10(times[0]/86400)), 1)
 for seconds in times:
     minutes = seconds // 60
     hours = minutes // 60
     days = hours // 24
-    print(str(days).zfill(x), "{:02d}".format(hours % 24), "{:02d}".format(minutes % 60), "{:02d}".format(seconds % 60), sep=':')
+    print(str(days).zfill(x), "{:02d}".format(hours % 24),
+          "{:02d}".format(minutes % 60), "{:02d}".format(seconds % 60),
+          sep=':')
